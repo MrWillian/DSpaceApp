a0a464dd4c56a7d71e98acaa8aa0bba807a9128c
MrWilllian <willian.dev10@gmail.com>
2020-04-13 15:09:16 -0300

    refactor: add onPress event

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,9 +3,11 @@ import { TouchableOpacity, Text } from 'react-native';
 
 import styles from './styles';
 
-export default function Button({ type, text }) {
+export default function Button({ type, text, onPress }) {
   return (
-    <TouchableOpacity style={[styles.container, type == 'Login' ? styles.loginBtn : styles.registerBtn]}>
+    <TouchableOpacity 
+      style={[styles.container, type == 'Login' ? styles.loginBtn : styles.registerBtn]}
+      onPress={onPress}>
       <Text style={[styles.text, type == 'Login' ? styles.loginText : styles.registerText]}>
         {text}
       </Text>
