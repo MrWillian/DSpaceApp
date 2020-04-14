70bb1f91708bb3e148a65fd296f141f7c715c4fb
MrWilllian <willian.dev10@gmail.com>
2020-04-14 09:56:44 -0300

    style: add static width and height to button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,10 +3,10 @@ import { TouchableOpacity, Text } from 'react-native';
 
 import styles from './styles';
 
-export default function Button({ type, text, onPress }) {
+export default function Button({ type, text, onPress, buttonStyle }) {
   return (
     <TouchableOpacity 
-      style={[styles.container, type == 'Login' ? styles.loginBtn : styles.registerBtn]}
+      style={[buttonStyle, styles.container, type == 'Login' ? styles.loginBtn : styles.registerBtn]}
       onPress={onPress}>
       <Text style={[styles.text, type == 'Login' ? styles.loginText : styles.registerText]}>
         {text}
