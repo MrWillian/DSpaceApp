3349f4b274c5ae327fd418c6a7b8d97df394f9db
MrWilllian <willian.dev10@gmail.com>
2020-04-14 15:38:27 -0300

    refactor: change login and register types to primary and secundary types

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,13 +4,19 @@ import { TouchableOpacity, Text } from 'react-native';
 import styles from './styles';
 
 export default function Button({ type, text, onPress, buttonStyle }) {
+  const PRIMARY = 'Primary';
+
   return (
     <TouchableOpacity 
-      style={[buttonStyle, styles.container, type == 'Login' ? styles.loginBtn : styles.registerBtn]}
+      style={[buttonStyle, styles.container, 
+        type == PRIMARY ? styles.primaryBtn : styles.secundaryBtn]}
       onPress={onPress}>
-      <Text style={[styles.text, type == 'Login' ? styles.loginText : styles.registerText]}>
+
+      <Text style={[styles.text, 
+        type == PRIMARY ? styles.primaryText : styles.secundaryText]}>
         {text}
       </Text>
+      
     </TouchableOpacity>
   );
 }
