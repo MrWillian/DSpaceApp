aa3e8be61803bcf41df5646f4ba7db6c9cfa673c
MrWilllian <willian.dev10@gmail.com>
2020-04-15 09:35:45 -0300

    refactor: add fab bottom buttons

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,6 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { styles } from './styles';
-import { colors } from 'react-native-elements';
 
 export default function Main() {
   return (
@@ -15,6 +14,17 @@ export default function Main() {
         </View>
         <TouchableOpacity style={styles.menuDrawerButton}>
           <Icon style={styles.menuIcon} name="bars" size={15} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.containerBottomButtons}>
+        <TouchableOpacity style={styles.btnLeft}>
+          <Icon style={styles.icon} name="bookmark" size={18} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnMiddle}>
+          <Icon style={styles.icon} name="map-marker" size={36} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnRight}>
+          <Icon style={styles.icon} name="user" size={18} />
         </TouchableOpacity>
       </View>
     </View>
